2b93f3f3ceb4b5d107b73edc93e34cee17b4027c
Mitsuka Kiyohara <mitsuka@menlohacks.com>
2023-09-11 15:49:40 -0400

    add Main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    // type psvm to generate main method
+    public static void main(String[] args) {
+        // don't forget semicolons! >:(
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main();
+    }
+}
